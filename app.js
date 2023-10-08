876640e00532784c8db1a2c5c97c42287aa66cb0
Maestro Haryor <ayomide.odewale1@gmail.com>
2023-10-08 09:15:59 +0100

    Day 1

---
+++ app.js
@@ -0,0 +1,134 @@
+// // DATATYPES
+
+// // Numbers
+// let num = 1032.27827272;
+// console.log(num.toFixed(2), typeof num);
+
+// // Strings
+// let myName = "John";
+// let address = "123 Main St";
+
+// console.log(myName);
+// console.log(address);
+
+// console.log(myName + " lives at " + address);
+// console.log(`${myName} lives at ${address}`);
+
+// // Boolean
+// let isTrue = true;
+// let isFalse = false;
+
+// console.log(isTrue, typeof isTrue);
+// console.log(isFalse, typeof isFalse);
+
+// // Undefined
+// let u;
+// console.log(u, typeof u);
+
+// // Null
+// let n = null;
+// console.log(n, typeof n);
+
+// // Arrays
+// let arrOfNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+// let arrOfStrings = ["Maestro", "lifetechfacts"];
+
+// console.log(arrOfNums);
+// console.log(arrOfStrings);
+
+// arrOfNums[1] = 20;
+
+// console.log(arrOfNums);
+
+// // Objects
+// let person = {
+//   name: "John",
+//   age: 30,
+//   address: "123 Main St",
+// };
+
+// console.log(
+//   `${person.name} is ${person.age} years old and lives at ${person.address}`
+// );
+
+// person.name = "Jane";
+// person.address = "456 Main St";
+// person.age = 40;
+
+// console.log(
+//   `${person.name} is ${person.age} years old and lives at ${person.address}`
+// );
+
+// JAVASCRIPT OPERATORS
+// let x = 10;
+// let y = 20;
+// let a = 5;
+// let b = 2;
+// let j = "Jane";
+// let k = "5";
+
+// Arithmetic Operators
+
+// console.log(x + y);
+// console.log(x - y);
+// console.log(x * y);
+// console.log(x ** y);
+// console.log(x / y);
+// console.log(a % b);
+// x++;
+// x++;
+// console.log(x);
+
+// x--;
+// console.log(x);
+// console.log(x++);
+// console.log(x--);
+
+// Assignment Operators
+// let x2 = x;
+// x2 += 2;
+// x2 -= 2;
+// x2 /= 2;
+
+// console.log(x2);
+
+// Comparison Operators
+// console.log(a !== k);
+
+// Logical Operators
+// let age = 18;
+// let country = "Nigeriasss";
+// console.log(age >= 18 && country === "Nigeria");
+// console.log(age >= 18 || country === "Nigeria");
+// console.log(!(age >= 18 || country === "Nigeria"));
+
+// ORDER OF OPERATIONS
+/* BEDMAS is an acronym that represents the order of operations in mathematics and stands for:
+
+    Brackets: Evaluate expressions inside parentheses or brackets first. This includes any innermost brackets or parentheses.
+
+    Exponents: Evaluate expressions with exponents (or powers) next. This involves raising a number to a power, such as squaring or cubing.
+
+    Division and Multiplication: Perform multiplication and division operations from left to right. These operations have the same level of precedence and are executed before addition and subtraction.
+
+    Addition and Subtraction: Finally, perform addition and subtraction operations from left to right. Like multiplication and division, these operations have the same level of precedence and are executed last. */
+// console.log(2 * (3 + 4));
+
+// Type Conversion
+// let x = 10;
+// let y = "5";
+// let result = x + y;
+// console.log(result, typeof result);
+// parseInt("42");
+// 42;
+// String(29292929);
+// ("29292929");
+// Boolean(29292929);
+// true;
+// Boolean(0);
+// false;
+// Boolean(1);
+// true;
+// Boolean("sgsgsgsgsgsgsgsgsgsg");
+// true;
